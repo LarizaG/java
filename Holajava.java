ca83cbe061f246e1ee40d0566c103d0f6631ed77
Astrti Lariza Garcia Castillo <90290485+LarizaG@users.noreply.github.com>
2021-10-05 20:38:10 -0500

    Hola mundo
    Hola mundo en java

---
+++ Holajava.java
@@ -0,0 +1,17 @@
+public class Holajava{
+	   /**
+	    *Aplicacion java
+	    hTitle       :Hola mundo en java
+	    *Description :Hola mundo en java desde vim
+	    *Author      :Astrit Lariza Garcia Castillo
+	    *Date        :20211105
+	    *Version     :1
+	    *Usage       :c:\Users\Dell\Documentos\holajava
+	    *
+	    *Hola mundo en java */
+
+	   public static void main(String []args){
+		   
+		   System.out.println("Hola mundo"); // prints hola mundo
+	   } 
+}  
